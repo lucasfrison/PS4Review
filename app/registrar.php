9df650d7fe3e4238c164df36bc19e20554a950ea
lucas <lucfg15@gmail.com>
2022-04-29 14:17:05 -0300

    Banco de dados criado

--- app/registrar.php
+++ app/registrar.php
@@ -2,7 +2,7 @@
   ini_set('display_errors', 1);
   ini_set('display_startup_errors', 1);
   error_reporting(E_ALL);
-  require "validation/check_form.php";
+  require "validation/check_form_register.php";
 ?>
 <!DOCTYPE html>
 <html>
