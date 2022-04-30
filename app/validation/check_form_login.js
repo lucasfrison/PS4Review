3024096c84aaaad66275483663bd6593a2284126
lucas <lucfg15@gmail.com>
2022-04-30 10:37:14 -0300

    login de usuario funcionando

--- app/validation/check_form_login.js
+++ app/validation/check_form_login.js
@@ -1,7 +1,7 @@
 $(function(){
     $("#form-test").on("submit",function(){
       email_input = $("input[name='email']");
-      senha_input = $("input[name='senha']");
+      senha_input = $("input[name='password']");
   
       if(email_input.val() == "" || email_input.val() == null)
       {
